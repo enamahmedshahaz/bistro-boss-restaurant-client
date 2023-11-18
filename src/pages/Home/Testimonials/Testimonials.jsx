fba69b8bf3bee63b52a0e49a60d6cb783964a006
enamahmedshahaz <enamahmedshahaz@gmail.com>
2023-11-18 19:17:04 +0600

    Add Menu Page

--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -42,7 +42,7 @@ const Testimonials = () => {
                         </div>
                     </SwiperSlide>)}
             </Swiper>
-            
+
         </section>
     );
 };
---
+++ src/pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,10 @@
+
+const Menu = () => {
+    return (
+        <div>
+            This is menu page
+        </div>
+    );
+};
+
+export default Menu;
