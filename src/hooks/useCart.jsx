bc24fcb674649369d24c35653c01129431c0d202
enamahmedshahaz <enamahmedshahaz@gmail.com>
2023-11-23 18:02:47 +0600

    modify useCart hook to get get cart based on user email

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,18 +1,21 @@
 import {
     useQuery,
-} from '@tanstack/react-query'
+} from '@tanstack/react-query';
+
+import { useAuth } from "../hooks/useAuth";
 
 import useAxiosSecure from './useAxiosSecure';
 
 const useCart = () => {
 
     const axiosSecure = useAxiosSecure();
+    const { user } = useAuth();
 
     const { data: cart = [] } = useQuery(
         {
-            queryKey: ['cart'],
+            queryKey: ['cart', user?.email],
             queryFn: async () => {
-                const res = await axiosSecure.get('/carts');
+                const res = await axiosSecure.get(`/cart?email=${user.email}`);
                 return res.data;
             },
         });
