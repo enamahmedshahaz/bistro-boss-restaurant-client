3ce2ebdd96c74f64649857d65fa154f98d63a819
enamahmedshahaz <enamahmedshahaz@gmail.com>
2023-11-24 16:21:47 +0600

    Add menu links for admin users on dashboard page

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { FaCalendar, FaCartShopping, FaHouse, FaHouseChimney, FaList, FaShop, FaStar } from "react-icons/fa6";
+import { FaBook, FaCalendar, FaCartShopping, FaHouse, FaHouseChimney, FaList, FaShop, FaSpoon, FaStar, FaUser, FaUsers, FaUtensils } from "react-icons/fa6";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../hooks/useCart";
 
@@ -7,6 +7,9 @@ const Dashboard = () => {
 
     const [cart] = useCart();
 
+    //Todo: 
+    const isAdmin = true;
+
     return (
         <div className="flex">
 
@@ -14,21 +17,47 @@ const Dashboard = () => {
             <div className="w-64 bg-yellow-500 min-h-screen">
                 <ul className="menu flex flex-col gap-2 uppercase">
 
-                    <li ><NavLink to="/dashboard/userHome">
-                        <FaHouse></FaHouse> User Home</NavLink></li>
+                    {isAdmin ?
+                        <>
+                        {/* Menu links for Admin Users */}
+                            <li><NavLink to="/dashboard/adminHome">
+                                <FaHouse></FaHouse> Admin Home</NavLink></li>
 
-                    <li><NavLink to="/dashboard/reservation">
-                        <FaCalendar></FaCalendar> Reservation </NavLink></li>
+                            <li><NavLink to="/dashboard/addItems">
+                                <FaUtensils></FaUtensils>Add Items</NavLink></li>
 
-                    <li><NavLink to="/dashboard/cart">
-                        <FaCartShopping></FaCartShopping>My Cart ({cart.length})</NavLink></li>
+                            <li><NavLink to="/dashboard/manageItems">
+                                <FaSpoon></FaSpoon>Manage Items</NavLink></li>
 
-                    <li><NavLink to="/dashboard/review">
-                        <FaStar></FaStar> Add Review</NavLink></li>
+                            <li><NavLink to="/dashboard/manageBookings">
+                                <FaBook></FaBook>Manage Bookings</NavLink></li>
 
-                    <li><NavLink to="/dashboard/bookings">
-                        <FaList></FaList> My Bookings</NavLink></li>
+                            <li><NavLink to="/dashboard/allUsers">
+                                <FaUsers></FaUsers>All Users</NavLink></li>
+                        </>
+                        :
+                        <>  
+                         {/* Menu links for Normal Users */}
+                        <li ><NavLink to="/dashboard/userHome">
+                            <FaHouse></FaHouse> User Home</NavLink></li>
 
+                            <li><NavLink to="/dashboard/reservation">
+                                <FaCalendar></FaCalendar> Reservation </NavLink></li>
+
+                            <li><NavLink to="/dashboard/cart">
+                                <FaCartShopping></FaCartShopping>My Cart ({cart.length})</NavLink></li>
+
+                            <li><NavLink to="/dashboard/review">
+                                <FaStar></FaStar> Add Review</NavLink></li>
+
+                            <li><NavLink to="/dashboard/bookings">
+                                <FaList></FaList> My Bookings</NavLink></li>
+                        </>
+                    }
+
+
+
+                    {/* Shared Menu Links for all*/}
                     <div className="divider"></div>
 
                     <li><NavLink to="/">
